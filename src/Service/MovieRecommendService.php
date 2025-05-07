108f46c8552a195e6a50f9ce57e8c164aba44393
radix505 <radix505@gmail.com>
2025-05-07 15:01:47 +0200

    Implemented base tests

--- src/Service/MovieRecommendService.php
+++ src/Service/MovieRecommendService.php
@@ -4,4 +4,18 @@ namespace App\Service;
 
 class MovieRecommendService
 {
+    public function recommend3RandomMovies(): array
+    {
+
+    }
+
+    public function recommendByLetterWAndEvenLength(): array
+    {
+
+    }
+
+    public function recommendMultiWordTitles(): array
+    {
+
+    }
 }
--- tests/Service/MovieRecommendServiceTest.php
+++ tests/Service/MovieRecommendServiceTest.php
@@ -2,26 +2,46 @@
 
 namespace App\Tests\Service;
 
+use App\Service\MovieRecommendService;
 use PHPUnit\Framework\TestCase;
 
 final class MovieRecommendServiceTest extends TestCase
 {
+    private const string MOVIES_FILE_PATH = __DIR__ . '/../../src/External/movies.php';
+    private array $movies;
+    private MovieRecommendService $service;
+
     protected function setUp(): void
     {
+        $this->movies = require self::MOVIES_FILE_PATH;
     }
 
     public function testRecommend3RandomMovies()
     {
+        $results = $this->service->recommend3RandomMovies();
 
+        $this->assertCount(3, $results);
+        foreach ($results as $title) {
+            $this->assertContains($title, $this->movies);
+        }
     }
 
     public function testRecommendByLetterWAndEvenLength()
     {
+        $results = $this->service->recommendByLetterWAndEvenLength();
 
+        foreach ($results as $title) {
+            $this->assertStringStartsWith('W', $title);
+            $this->assertEquals(0, strlen($title) % 2);
+        }
     }
 
     public function testRecommendMultiWordTitles()
     {
+        $results = $this->service->recommendMultiWordTitles();
 
+        foreach ($results as $title) {
+            $this->assertGreaterThan(1, str_word_count($title));
+        }
     }
 }
