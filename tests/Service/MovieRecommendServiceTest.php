28076cd09112c452cac3a97746268954edf11057
radix505 <radix505@gmail.com>
2025-05-07 12:51:19 +0000

    Initial commit with structure

---
+++ tests/Service/MovieRecommendServiceTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Tests\Service;
+
+use PHPUnit\Framework\TestCase;
+
+final class MovieRecommendServiceTest extends TestCase
+{
+    protected function setUp(): void
+    {
+    }
+
+    public function testRecommend3RandomMovies()
+    {
+
+    }
+
+    public function testRecommendByLetterWAndEvenLength()
+    {
+
+    }
+
+    public function testRecommendMultiWordTitles()
+    {
+
+    }
+}
